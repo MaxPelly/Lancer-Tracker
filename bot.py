82b3417a8dc0e5df5b4107fd06e36af0f15de36a
Max Pelly <maximpely@gmail.com>
2023-02-12 17:12:52 +0000

    clean up

--- bot.py
+++ bot.py
@@ -20,8 +20,10 @@ import logging
 import typing
 from threading import Lock
 from Player import Player
-import logging
 
+
+import requests
+from lxml import etree as ET
 import configparser
 from os.path import exists
 
@@ -58,20 +60,47 @@ bot.not_implemented_error = "Opps, im not here yet!"
 
 
 async def check_channel_set(ctx):
+    """
+    Check if the bot channel has been set. If not reply with a relevant error message
+    """
     if not bot.main_channel:
         names = [channel.name for channel in ctx.guild.channels if type(channel) == discord.channel.TextChannel]
         name_text = "\n- ".join(names)
         await ctx.reply(f"Using this function requires setting the bot channel. Please reply \"{bot.command_prefix}set_channel <channel name>\".\nAvaliable channels:\n- {name_text}", delete_after=10)
         return False
     return True
-    
+
 
 async def check_channel_or_dm(ctx):
+    """
+    Check if the bot has been called in its designated channel or a DM
+    """
     if (ctx.channel.id == bot.main_channel) or (type(ctx.channel) == discord.channel.DMChannel):
         return True
-    
+
     await check_channel_set(ctx)
     return False
+
+
+@bot.command(alias=['r'])
+async def relay(
+    ctx
+):
+    """
+    Mystery Biscuits
+    """
+    version_one = set(requests.get("https://www.relay.fm/robots.txt").text.split("\n"))
+    version_two = set(requests.get("https://www.relay.fm/robots.txt").text.split("\n"))
+
+    repeats = version_one.intersection(version_two)
+    urls = ["https://www.relay.fm" + i.split(" ")[1] + ".rss" for i in repeats if "feed" in i]
+
+    out = ""
+    for url in urls:
+        feed = ET.fromstring(requests.get(url).text)
+        title = feed.find("channel").find("title").text
+        out += f"{title}: {feed}\n"
+    ctx.reply(out)
 
 
 @bot.command(aliases=['sc'])
@@ -85,15 +114,15 @@ async def set_channel(
     """
     Set the channel to bot will listen in and share information in.
     """
-    
+
     await ctx.reply(channel)
     if len(channel) == 0:
         await ctx.reply(f"{bot.incorect_args_error} please provide the name of the channel")
         return
-    names = {channel.name:channel.id for channel in ctx.guild.channels if type(channel) == discord.channel.TextChannel}
+    names = {channel.name: channel.id for channel in ctx.guild.channels if type(channel) == discord.channel.TextChannel}
     if channel_id := names.get(channel, False):
         await ctx.reply(f"Setting Channel to {channel}")
-        bot.main_channel=channel_id
+        bot.main_channel = channel_id
         channel = bot.get_channel(channel_id)
         with bot.config.lock:
             bot.config.set("bot", "main_channel", str(channel_id))
@@ -103,12 +132,12 @@ async def set_channel(
         channel_names = "\n- ".join(names.keys())
         await ctx.reply(f"\"{channel}\" does not appear to be a valid channel.\nAvaliable channels are:\n- {channel_names}")
 
-        
+
 @bot.command(aliases=['i'])
 @commands.check(check_channel_or_dm)
 async def init(
     ctx,
-    manna: typing.Optional[int]=commands.parameter(
+    manna: typing.Optional[int] = commands.parameter(
         default=0,
         description="Starting Manna"
     ),
@@ -123,22 +152,22 @@ async def init(
     Callsigns can not start with a number followed by a space.
     If you really want this use a dummy here then run update_callsign
     """
-    
+
     if mech := Player.get_player_by_name(ctx.author.id):
         await ctx.reply(f"You already have a mech: {mech}. Run \"{bot.command_prefix}delete\" to remove it.")
         return
-    
+
     new_player = Player(name=ctx.author.id, callsign=callsign, manna=manna)
     await ctx.reply(f"Created new mech.\n{new_player}")
 
-    
+
 @bot.command()
 @commands.check(check_channel_or_dm)
 async def delete(ctx):
     """
     Delete your record. WARNING IREVERSIBLE.
     """
-    
+
     if mech := Player.get_player_by_name(ctx.author.id):
         callsign = mech.get("callsign")
         mech.delete(confirm=True)
@@ -146,7 +175,7 @@ async def delete(ctx):
     else:
         await ctx.reply(bot.no_mech_error)
 
-        
+
 @bot.command(aliases=['c'])
 @commands.check(check_channel_or_dm)
 async def check(ctx):
@@ -157,13 +186,14 @@ async def check(ctx):
         await ctx.reply(f"{mech}")
     else:
         await ctx.reply(bot.no_mech_error)
-        
+
+
 @bot.command(aliases=['uc'])
 @commands.check(check_channel_or_dm)
 async def update_callsign(
     ctx,
     *,
-    callsign:str = commands.parameter(
+    callsign: str = commands.parameter(
         description="The new callsign"
     ),
 ):
@@ -176,7 +206,6 @@ async def update_callsign(
         await ctx.reply(f"{mech}")
     else:
         await ctx.reply(bot.no_mech_error)
-
 
 
 @bot.command(aliases=['b'])
@@ -192,28 +221,28 @@ async def buy(
     Spend some Manna.
     Options are l[icence], ta[lent] or tr[aining].
     """
-    
+
     if mech := Player.get_player_by_name(ctx.author.id):
         reply = []
         for request in items.split(" "):
             if request[0] == "l":
                 sucess, res = mech.buy_licence()
                 if sucess:
-                    reply.append(f"Sucesfully bought a licence.")
+                    reply.append("Sucesfully bought a licence.")
                 else:
-                    reply.append(f"Unable to buy a licence. {res}")
-                    
+                    reply.append("Unable to buy a licence. {res}")
+
             elif len(request) > 1 and request[:2] == "ta":
                 sucess, res = mech.buy_talent()
                 if sucess:
-                    reply.append(f"Sucesfully bought a talent.")
+                    reply.append("Sucesfully bought a talent.")
                 else:
                     reply.append(f"Unable to buy a talent. {res}")
-                    
+
             elif len(request) > 1 and request[:2] == "tr":
                 sucess, res = mech.buy_training()
                 if sucess:
-                    reply.append(f"Sucesfully bought training.")
+                    reply.append("Sucesfully bought training.")
                 else:
                     reply.append(f"Unable to buy training. {res}")
             else:
@@ -221,11 +250,9 @@ async def buy(
         reply.append("")
         reply.append(str(mech))
         await ctx.reply("\n".join(reply))
-        return
     else:
         await ctx.reply(bot.no_mech_error)
-        return
-    
+
 
 @bot.command(aliases=['cm'])
 @commands.dm_only()
@@ -248,21 +275,20 @@ async def complete_mission(
         gm_mech.give_manna(bot.gm_manna)
     else:
         await ctx.send(f"Unable to award GM Manna. {bot.no_mech_error}")
-    
+
     for player in players:
         if mech := Player.get_player_by_name(player.id):
             await ctx.send(f"giving {manna} Manna to {player.name}")
             mech.give_manna(manna)
         else:
-            #await player.send(f"You completed a Lancer mission that should have awarded {manna} Manna! {bot.no_mech_error} Add ;{manna} after your callsign to include this missed manna.")
+            # await player.send(f"You completed a Lancer mission that should have awarded {manna} Manna! {bot.no_mech_error} Add ;{manna} after your callsign to include this missed manna.")
             await ctx.send(f"Unable to award {player.name} Manna.")
-    
-    
+
     players = ", ".join(player.mention for player in players)
     channel = bot.get_channel(bot.main_channel)
     await channel.send(f"A game run by {ctx.author.mention} completed attended by {players}. {manna} Manna is awarded.")
 
-    
+
 @bot.command(aliases=['pcm'])
 @commands.dm_only()
 @commands.check(check_channel_set)
@@ -275,11 +301,11 @@ async def player_complete_mission(
         description="@<the GM>"
     ),
     players: commands.Greedy[discord.Member] = commands.parameter(
-        description= "List of @<player>"
+        description="List of @<player>"
     )
 ):
     """
-    Awards Manna for a mission where you were a player. 
+    Awards Manna for a mission where you were a player.
     Includes GM manna.
     No need to @ yourself.
     """
@@ -288,38 +314,35 @@ async def player_complete_mission(
         gm_mech.give_manna(100)
     else:
         await ctx.send(f"Unable to award GM Manna to {gm.name}.")
-    
+
     for player in players:
         if mech := Player.get_player_by_name(player.id):
             await ctx.send(f"giving {manna} Manna to {player.name}")
             mech.give_manna(manna)
         else:
-            #await player.send(f"You completed a Lancer mission that should have awarded {manna} Manna! {bot.no_mech_error} Add ;{manna} after your callsign to include this missed manna.")
+            # await player.send(f"You completed a Lancer mission that should have awarded {manna} Manna! {bot.no_mech_error} Add ;{manna} after your callsign to include this missed manna.")
             await ctx.send(f"Unable to award {player.name} Manna. {bot.no_mech_error}")
-    
-    
+
     player_names = ", ".join(player.mention for player in players)
-    if (not ctx.author in players):
-        if (your_mech := Player.get_player_by_name(ctx.author.id)):
+    if ctx.author not in players:
+        if your_mech := Player.get_player_by_name(ctx.author.id):
             await ctx.send(f"giving {manna} Manna to {ctx.author.name}")
             your_mech.give_manna(manna)
         else:
             await ctx.send(f"Unable to award you Manna. {bot.no_mech_error}")
-        
+
         player_names += f", {ctx.author.mention}"
-    
-    
-    
+
     channel = bot.get_channel(bot.main_channel)
     await channel.send(f"A mission by {gm.mention} was sucesfully completed by {player_names}. {manna} Manna is awarded.")
 
-    
+
 @bot.command(aliases=['p'])
 @commands.check(check_channel_or_dm)
 async def ping(ctx):
     """
     pong
-    
+
     :param args: some args
     """
     await ctx.reply('pong')
@@ -327,6 +350,9 @@ async def ping(ctx):
 
 @bot.event
 async def on_command_error(ctx, error):
+    """
+    Custom error messaging
+    """
     if isinstance(error, commands.BadArgument):
         await ctx.reply(f"Incorect Arguments. Please check the \"{bot.command_prefix}help\" command for more information.", delete_after=10)
     elif isinstance(error, commands.CommandNotFound):
@@ -335,7 +361,7 @@ async def on_command_error(ctx, error):
         await ctx.reply("This command is only avaliable in DMs to keep things clean.", delete_after=10)
     else:
         await ctx.reply(f"{error}", delete_after=30)
-    
+
     # THERE HAS GOT TO BE A BETTER WAY TO DO THIS!!!!
     await bot.err(ctx, error)
 
